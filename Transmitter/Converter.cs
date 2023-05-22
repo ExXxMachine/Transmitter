df1571345da655af0c6ad741414df2969fdef61d
ExXxMachine <105457858+ExXxMachine@users.noreply.github.com>
2023-05-22 11:54:18 +0600

    Добавьте файлы проекта.

---
+++ Transmitter/Converter.cs
@@ -0,0 +1,37 @@
+﻿namespace Transmitter;
+
+public static class Converter
+{
+    private const string Directory = "../../../";
+
+    public static List<string> ToBinary(string path)
+    {
+        var binary = new List<string>();
+        var test = GetFileAsBytes(Directory + path);
+        foreach (var item in test)
+        {
+            var currentByte = Convert.ToString(item, 2);
+            currentByte = GetFullByte(currentByte);
+            binary.Add(GetFullByte(currentByte));
+            Console.WriteLine(currentByte);
+        }
+
+        return binary;
+    }
+
+    private static string GetFullByte(string b)
+    {
+        var result = "";
+        for (var i = 0; i < 8 - b.Length; i++)
+        {
+            result += '0';
+        }
+        result += b;
+        return result;
+    }
+
+    private static byte[] GetFileAsBytes(string filename)
+    {
+        return File.ReadAllBytes(filename);
+    }
+}
---
+++ Transmitter/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Versioning;
+namespace Transmitter;
+[SupportedOSPlatform("windows")]
+public static class Program
+{
+    public static void Main()
+    {
+        var player = new Player();
+        var result = Converter.ToBinary("popa.txt");
+        Console.WriteLine("\n" + result.Count);
+        var transmitter = new Transmitter(player, result);
+        transmitter.Start();
+    }
+}
